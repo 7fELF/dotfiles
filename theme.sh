8778b11e8d64dc0bcd64153cf3024de931d41727
Antoine Baudrand <antoine.baudrand@epitech.eu>
2016-12-07 18:08:15 +0100

    Add script to install theme

---
+++ theme.sh
@@ -0,0 +1,12 @@
+sudo apt install gnome-tweak-tool
+mkdir $HOME/.themes
+cd $HOME/.themes
+git clone https://github.com/anmoljagetia/Flatabulous.git
+gsettings set org.gnome.desktop.interface gtk-theme "Flatabulous"
+
+# TODO: change that
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install ultra-flat-icons
+
+gsettings set org.gnome.desktop.interface icon-theme "ultra-flat-icons"

871afb0141748a380205c56b9f04242eedeaea89
Antoine Baudrand <antoine.baudrand@epitech.eu>
2016-12-09 10:31:34 +0100

    Set Wallpaper in theme.sh

--- theme.sh
+++ theme.sh
@@ -15,6 +15,9 @@ sudo chsh $USER -s $(which zsh)
 gsettings set org.gnome.desktop.interface icon-theme "Ultra-Flat"
 cat ./org.compiz.profiles.unity.plugins.grid.dconf | dconf load
 
+wget https://breakingmen.files.wordpress.com/2014/02/300002.jpg -O $HOME/w.jpg
+gsettings set org.gnome.desktop.background picture-uri "file://$HOME/w.jpg"
+
 mkdir -p $HOME/.fonts
 cd $HOME/.fonts
 git clone https://github.com/powerline/fonts.git

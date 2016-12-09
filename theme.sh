0429a9c8d7784e8816b735bb7295be145366dab4
Antoine Baudrand <antoine.baudrand@epitech.eu>
2016-12-09 10:12:38 +0100

    Install terminator and set default shell to zsh

--- theme.sh
+++ theme.sh
@@ -7,7 +7,10 @@ gsettings set org.gnome.desktop.interface gtk-theme "Flatabulous"
 # TODO: change that
 sudo add-apt-repository ppa:noobslab/icons
 sudo apt-get update
-sudo apt-get install ultra-flat-icons
+sudo apt-get install -y ultra-flat-icons
+
+sudo apt-get install -y terminator
+sudo chsh $USER -s $(which zsh)
 
 gsettings set org.gnome.desktop.interface icon-theme "Ultra-Flat"
 cat ./org.compiz.profiles.unity.plugins.grid.dconf | dconf load

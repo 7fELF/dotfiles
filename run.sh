5d230f36d778600488766d936ff303878e55ef4a
Antoine Baudrand <antoine.baudrand@epitech.eu>
2017-01-09 10:43:52 +0100

    Indent run.sh

--- run.sh
+++ run.sh
@@ -4,13 +4,14 @@ set -x
 DOTFILES_FOLDER="$PWD/$(dirname $0)"
 LN="ln -sf"
 
-$LN $DOTFILES_FOLDER/vimrc		    $HOME/.vimrc
-$LN $DOTFILES_FOLDER/tmux.conf	    $HOME/.tmux.conf
-$LN $DOTFILES_FOLDER/zshrc		    $HOME/.zshrc
-$LN $DOTFILES_FOLDER/editorconfig	    $HOME/.editorconfig
-$LN $DOTFILES_FOLDER/gitconfig        $HOME/.gitconfig
-$LN $DOTFILES_FOLDER/vim              $HOME/.vim
-$LN $DOTFILES_FOLDER/selected_editor  $HOME/.selected_editor
+$LN $DOTFILES_FOLDER/vimrc              $HOME/.vimrc
+$LN $DOTFILES_FOLDER/tmux.conf          $HOME/.tmux.conf
+$LN $DOTFILES_FOLDER/zshrc              $HOME/.zshrc
+$LN $DOTFILES_FOLDER/editorconfig       $HOME/.editorconfig
+$LN $DOTFILES_FOLDER/gitconfig          $HOME/.gitconfig
+$LN $DOTFILES_FOLDER/vim                $HOME/.vim
+$LN $DOTFILES_FOLDER/selected_editor    $HOME/.selected_editor
+$LN $DOTFILES_FOLDER/ycm_extra_conf.py  $HOME/.ycm_extra_conf.py
 
 mkdir -p $HOME/.config/terminator/
 $LN $DOTFILES_FOLDER/config/terminator/config $HOME/.config/terminator/config

21cafd494f07c64d7666d2add162065c27d80bc4
Antoine Baudrand <antoine.baudrand@epitech.eu>
2017-01-25 11:38:40 +0100

    Add .icons folder in dotfiles

--- theme.sh
+++ theme.sh
@@ -1,19 +1,22 @@
-sudo apt install gnome-tweak-tool
+#/bin/sh
+set -x
+
+DOTFILES_FOLDER="$PWD/$(dirname $0)"
+LN="ln -sf"
+
+sudo apt-get install gnome-tweak-tool
 mkdir $HOME/.themes
 cd $HOME/.themes
 git clone https://github.com/anmoljagetia/Flatabulous.git
 gsettings set org.gnome.desktop.interface gtk-theme "Flatabulous"
 
-# TODO: change that
-sudo add-apt-repository ppa:noobslab/icons
-sudo apt-get update
-sudo apt-get install -y ultra-flat-icons
+$LN $DOTFILES_FOLDER/icons              $HOME/.icons
 
 sudo apt-get install -y terminator
 sudo chsh $USER -s $(which zsh)
 
 gsettings set org.gnome.desktop.interface icon-theme "Ultra-Flat"
-cat ./org.compiz.profiles.unity.plugins.grid.dconf | dconf load
+# cat ./dconf.dump | dconf load
 
 wget https://breakingmen.files.wordpress.com/2014/02/300002.jpg -O $HOME/w.jpg
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/w.jpg"

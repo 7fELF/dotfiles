154709218ecf73cf72b3692cfda138ce04dcd111
Antoine Baudrand <antoine.baudrand@epitech.eu>
2017-02-07 00:55:21 +0100

    theme.sh add link_folder function

--- theme.sh
+++ theme.sh
@@ -4,19 +4,21 @@ set -ex
 DOTFILES_FOLDER="$PWD/$(dirname "$0")"
 LN="ln -sf"
 
+function link_folder {
+  rm -rf "$HOME/.$1"
+  $LN "$DOTFILES_FOLDER/$1" "$HOME/.$1"
+}
+
 # Setup themes folder
 # Flatabulous is NOT compatible with GTK 3.20
 # so it's broken on Ubuntu 16.10 (for now)
-rm -rf "$HOME/.themes"
-$LN "$DOTFILES_FOLDER/themes" "$HOME/.themes"
+link_folder themes
 
 # Setup the icons folder
-rm -rf "$HOME/.icons"
-$LN "$DOTFILES_FOLDER/icons" "$HOME/.icons"
+link_folder icons
 
 # Setup the fonts folder
-rm -rf "$HOME/.fonts"
-$LN "$DOTFILES_FOLDER/fonts" "$HOME/.fonts"
+link_folder fonts
 
 # Install a few packages
 PACKAGES=(zsh terminator redshift vlc firefox)
@@ -32,7 +34,7 @@ echo "Installing oh-my-zsh"
 wget -qO- "https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh" | sh
 
 # Get our zshrc back
-$LN "$DOTFILES_FOLDER/zshrc"              "$HOME/.zshrc"
+$LN "$DOTFILES_FOLDER/zshrc" "$HOME/.zshrc"
 
 # Load my dconf dump
 dconf load / < "$DOTFILES_FOLDER/dconf.ini"

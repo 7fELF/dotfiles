d4870013bd1efbae44c660da3fd2bcc5f3f305c7
Antoine Baudrand <antoine.baudrand@epitech.eu>
2017-02-07 00:41:51 +0100

    Fix oh-my-sh bug in theme.sh

--- theme.sh
+++ theme.sh
@@ -5,41 +5,43 @@ DOTFILES_FOLDER="$PWD/$(dirname "$0")"
 LN="ln -sf"
 
 # Setup themes folder
+# Flatabulous is NOT compatible with GTK 3.20
+# so it's broken on Ubuntu 16.10 (for now)
 rm -rf "$HOME/.themes"
 $LN "$DOTFILES_FOLDER/themes" "$HOME/.themes"
-
-# Set the theme
-# Not compatible with GTK 3.20 so it's broken on Ubuntu 16.10
-gsettings set org.gnome.desktop.interface gtk-theme "Flatabulous"
 
 # Setup the icons folder
 rm -rf "$HOME/.icons"
 $LN "$DOTFILES_FOLDER/icons" "$HOME/.icons"
 
-# Set the icon theme
-gsettings set org.gnome.desktop.interface icon-theme "Ultra-Flat"
-
 # Install a few packages
 PACKAGES=(zsh terminator redshift vlc firefox)
 sudo apt-get install -y "${PACKAGES[@]}"
 
-echo "Installing oh-my-zsh"
-wget -qO- "https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh" | sh
-
 # Set zsh default shell
 sudo chsh "$USER" -s "$(which zsh)"
 
+# Avoid breaking the oh-my-zsh install script
+rm -rf "$HOME/.zshrc"
+
+echo "Installing oh-my-zsh"
+wget -qO- "https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh" | sh
+
+# Get our zshrc back
+$LN "$DOTFILES_FOLDER/zshrc"              "$HOME/.zshrc"
+
 # Install powerline fonts
-# TODO: use a submodule
+# TODO: use a submodule for that
 rm -rf "$HOME/.fonts"
 mkdir -p "$HOME/.fonts"
 cd "$HOME/.fonts"
 git clone https://github.com/powerline/fonts.git
 
+# Load my dconf dump
 dconf load / < "$DOTFILES_FOLDER/dconf.ini"
 
 # Set a Wallpaper
-WALLPAPER_URL="https://breakingmen.files.wordpress.com/2014/02/300002.jpg"
+WALLPAPER_URL="https://archive-media-1.nyafuu.org/wg/image/1473/13/1473131556085.jpg"
 wget "$WALLPAPER_URL" -O "$HOME/w.jpg"
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/w.jpg"
 

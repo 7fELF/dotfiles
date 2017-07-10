46189822b8755a34f799b2f60cef1f216b45e1ef
Antoine Baudrand <antoine.baudrand@epitech.eu>
2017-07-10 23:01:39 +0200

    Update github username

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Sets up my work environment with all my configuration files in only a few minute
 ## Quick install command
 :warning: Tested only on Ubuntu 16.04 :warning:
 ```
-curl -sSL https://raw.githubusercontent.com/antoinegergy/dotfiles/master/install.sh | sh
+curl -sSL https://raw.githubusercontent.com/7fELF/dotfiles/master/install.sh | sh
 ```
 or:
 ```
-wget -qO- https://raw.githubusercontent.com/antoinegergy/dotfiles/master/install.sh | sh
+wget -qO- https://raw.githubusercontent.com/7fELF/dotfiles/master/install.sh | sh
 ```
 
 ## TODO
--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-#   'curl -sSL https://raw.githubusercontent.com/antoinegergy/dotfiles/master/install.sh | sh'
+#   'curl -sSL https://raw.githubusercontent.com/7fELF/dotfiles/master/install.sh | sh'
 #   or:
-#   'wget -qO- https://raw.githubusercontent.com/antoinegergy/dotfiles/master/install.sh | sh'
+#   'wget -qO- https://raw.githubusercontent.com/7fELF/dotfiles/master/install.sh | sh'
 #
 set -ex
 
@@ -15,7 +15,7 @@ sudo apt-get update
 sudo apt-get install -y git
 
 cd "$HOME"
-git clone --recursive https://github.com/antoinegergy/dotfiles.git
+git clone --recursive https://github.com/7fELF/dotfiles.git
 cd dotfiles
 
 ./run.sh

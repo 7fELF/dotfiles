64bef9e8b710441561cacfee96076c1ea9052302
Antoine Baudrand <antoine.baudrand@epitech.eu>
2020-11-14 10:40:34 +0100

    update dropbox script

--- scripts/dropbox.sh
+++ scripts/dropbox.sh
@@ -4,12 +4,12 @@ set -ex
 # Install Dropbox
 
 # Add repo
-sudo bash -c 'echo "deb http://linux.dropbox.com/ubuntu xenial main" > /etc/apt/sources.list.d/dropbox.list'
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+sudo bash -c 'echo "deb http://linux.dropbox.com/ubuntu cosmic main" > /etc/apt/sources.list.d/dropbox.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
 
 # Install
 sudo apt update
-sudo apt install -y dropbox python-gpgme
+sudo apt install -y dropbox python3-gpg
 
 echo 'y' | env -i dropbox start -i
 dropbox stop
